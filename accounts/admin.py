da3a48a56c04be40a6b26d709867cf3936af6b34
LongHD <longquanthinh1@gmail.com>
2026-05-23 19:15:15 +0700

    Update

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.db.models import Sum
 from .models import CustomUser, ReferralPayout, ReferralSettings
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    change_list_template = 'admin/accounts/customuser/change_list.html'
+
     list_display = [
         'username', 'email', 'coins', 'is_email_verified',
         'referral_code', 'referred_by', 'referral_coins_earned', 'date_joined',
@@ -22,6 +25,31 @@ class CustomUserAdmin(UserAdmin):
         )}),
         ('OTP', {'fields': ('otp_code', 'otp_created_at'), 'classes': ('collapse',)}),
     )
+
+    def changelist_view(self, request, extra_context=None):
+        from deposits.models import DepositTransaction
+
+        total_deposited = (
+            DepositTransaction.objects
+            .filter(status=DepositTransaction.STATUS_COMPLETED)
+            .aggregate(total=Sum('coins_credited'))['total'] or 0
+        )
+        total_f1 = self.model.objects.filter(referred_by__isnull=False).count()
+        total_f2 = self.model.objects.filter(referred_by__referred_by__isnull=False).count()
+        total_commission = (
+            ReferralPayout.objects
+            .filter(status=ReferralPayout.STATUS_COMPLETED)
+            .aggregate(total=Sum('amount_coins'))['total'] or 0
+        )
+
+        extra_context = extra_context or {}
+        extra_context['overview_stats'] = {
+            'total_deposited': f'{total_deposited:,.2f}',
+            'total_f1': total_f1,
+            'total_f2': total_f2,
+            'total_commission': f'{total_commission:,.2f}',
+        }
+        return super().changelist_view(request, extra_context=extra_context)
 
 
 @admin.register(ReferralSettings)
